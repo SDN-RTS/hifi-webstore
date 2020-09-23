861a22de2094c16afb571798df87bba3c7f44e9d
Mikkel Frederiksen <5760459@rts-365.dk>
2020-09-23 11:35:47 +0200

    added basic price filtering to shopping list

---
+++ js-modules/price-filter.js
@@ -0,0 +1,52 @@
+function priceFilter() {
+    let itemsWrapper = document.querySelector("#shoppingList__showProductsList");
+    let priceBtns = document.querySelectorAll(".price-btns");
+    for (let i = 0; i < priceBtns.length; i++) {
+        let btn = priceBtns[i];
+        btn.addEventListener("click", function (event) {
+            event.preventDefault();
+            productSorter(event.target)
+        })
+    }
+    function getProducts() {
+        return fetch('./components.json')
+        .then(function (response) {
+            return response.json()
+        })
+        .then (function (data) {
+            data = data.products;
+            return data;
+            
+        })
+    }
+    function productSorter(btn) {
+        getProducts()
+        .then(function (products) {
+            itemsWrapper.innerHTML = "";
+            var regex = /[+-]?\d+(\.\d+)?/g;
+            let txt = btn.innerText;
+            var floats = txt.match(regex)
+            var minPrice = parseInt(floats[0]);
+            var maxPrice = parseInt(floats[1]);
+            products.forEach(product => {
+                if (product.price > minPrice && product.price < maxPrice){
+                    let clone = window.shoppingListTemplate.content.cloneNode(true);
+                    clone.querySelector(".shoppingList__img").src = product.image;
+                    clone.querySelector(".shoppingList__imgContainer").alt = product.category;
+                    clone.querySelector(".shoppingList__productName").innerText = product.name;
+                    clone.querySelector(".shoppingList__productName").href = "product-view.html?id=" + product.id;
+                    clone.querySelector(".shoppingList__priceTag").innerText += product.price;
+
+
+
+
+                    itemsWrapper.appendChild(clone)
+                }
+            });
+        })
+    }
+
+    
+}
+priceFilter()
+// export default priceFilter;
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+import priceFilter from './js-modules/price-filter.js';
 import slideShow from './js-modules/slideshow.js';
 slideShow();
+priceFilter();
 
