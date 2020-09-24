6adee558431a1c49f4521e71ed20aef8a51a1d2c
Mikkel Frederiksen <59828400+M1quel@users.noreply.github.com>
2020-09-24 10:54:31 +0200

    Update shoppingpage-fetch.js

--- js-modules/shoppingpage-fetch.js
+++ js-modules/shoppingpage-fetch.js
@@ -1,4 +1,5 @@
-    function getProducts() {
+    function fetchAll(){
+        function getProducts() {
         return fetch("./components.json")
             .then(function (response) {
                 return response.json()
@@ -6,37 +7,40 @@
             .then(function (components) {
                 return components.products;
             })
+        }
+
+
+        function showProducts(){
+            getProducts() 
+            .then(function (products) {
+                let shoppingListTemplate = window.shoppingList__showProductsList__template;
+                let shoppingList = document.querySelector("#shoppingList__showProductsList");
+
+
+                products.forEach(function(product) {
+                    let clone = shoppingListTemplate.content.cloneNode(true);
+                    let productName = clone.querySelector(".shoppingList__productName");
+                    let productImg = clone.querySelector(".shoppingList__img");
+                    let productPrice = clone.querySelector(".shoppingList__priceTag");
+                    let productItem = clone.querySelector(".shoppingList__productListing");
+
+                    // let productList = document.querySelector("#shoppingList__showProductsList");
+
+                    productName.innerText = product.name;
+                    productItem.href = "product-view.html?id=" + product.id;
+                    productImg.src = product.image;
+                    productPrice.innerText += product.price;
+
+
+                    shoppingList.appendChild(clone);
+                    });
+                })
+
+            };
+    
     }
+export default fetchAll;
 
-
-    function showProducts(){
-        getProducts() 
-        .then(function (products) {
-            let shoppingListTemplate = window.shoppingList__showProductsList__template;
-            let shoppingList = document.querySelector("#shoppingList__showProductsList");
-
-
-            products.forEach(function(product) {
-                let clone = shoppingListTemplate.content.cloneNode(true);
-                let productName = clone.querySelector(".shoppingList__productName");
-                let productImg = clone.querySelector(".shoppingList__img");
-                let productPrice = clone.querySelector(".shoppingList__priceTag");
-                let productItem = clone.querySelector(".shoppingList__productListing");
-                
-                // let productList = document.querySelector("#shoppingList__showProductsList");
-                
-                productName.innerText = product.name;
-                productItem.href = "product-view.html?id=" + product.id;
-                productImg.src = product.image;
-                productPrice.innerText += product.price;
-
-
-                shoppingList.appendChild(clone);
-                });
-            })
-            
-        };
-        showProducts()
     
 
 // document.querySelector(".shoppingList__productListing").addEventListener("click", function(event) {
