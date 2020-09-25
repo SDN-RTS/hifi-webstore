d365ab9c1e344056c379756ad4c8f3d1777796ef
Mikkel Frederiksen <59828400+M1quel@users.noreply.github.com>
2020-09-25 08:32:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ import productDetails from './js-modules/productDetails.js';
 productDetails();
 
 var urlParam = new URLSearchParams(window.location.search);
+urlParam.addEventListener("change", function(){
+    if (root == "shoppinglist.html" && !urlParam.get("category")){
+        console.log("det er nu den skal aktivere");
+        fetchAll();
+    }
+})
 
 var url = window.location.href;
 var root = url.split("/")[3]; //index.html
 
 if(root == "index.html"){
     slideShow();
-}
-if (root == "shoppinglist.html" && !urlParam.get("category")){
-    console.log("det er nu den skal aktivere");
-    fetchAll();
 }
